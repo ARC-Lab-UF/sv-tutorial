d4df93783ba2e2baa4f8b013d359aa3062e80523
Greg Stitt <gstitt@ece.ufl.edu>
2021-12-14 20:13:47 +0000

    Commented and polished add example.

--- priority_encoder/add.sv
+++ priority_encoder/add.sv
@@ -1,6 +1,12 @@
+// Greg Stitt
+// University of Florida
+
+// Module: add
+// Description: A basic adder with a parameter for the width of the inputs,
+//              which generates a sum of the same width.
 module add
   #(
-    parameter WIDTH = 16
+    parameter WIDTH
     )
    (
     input logic [WIDTH-1:0]  in0, in1,
@@ -12,6 +18,10 @@ module add
 endmodule
 
 
+// Module: add_carry_out_bad
+// Description: An extension of the previous adder to provide a carry out,
+//              but done in a way that will not simulate correctly.
+
 module add_carry_out_bad
   #(
     parameter WIDTH = 16
@@ -22,16 +32,32 @@ module add_carry_out_bad
     output logic 	     carry_out
     );
 
+   // Use an internal variable to store the WIDTH+1 bit sum, which will be
+   // sliced to form the sum and carry out.
    logic [WIDTH:0] 	     full_sum;   
    
    always_comb begin
-      full_sum <= {1'b0,in0} + in1;     
+      // This incorrect implementation uses a non-block assignment to full_sum,
+      // which is only updated at the end of the always block. Non-blocking
+      // assignments cause subsequent accesses to the variable to use the
+      // previous value. When simulated, sum and carry_out will reflect the
+      // inputs from the previous inputs, not the current inputs.
+      //
+      // VHDL COMPARISON: non-blocking assignments are similar to signal
+      // assignments, and block assignments are similar to variable assignments.
+      // One nice feature of SV (or Verilog) is that you don't need to declare
+      // things differently. Everything is a variable and you can choose
+      // different assignments in different contexts.
+      full_sum <= in0 + in1;     
       sum <= full_sum[WIDTH-1:0];
       carry_out <= full_sum[WIDTH];      
    end
    
-endmodule
+endmodule // add_carry_out_bad
 
+
+// Module: add_carry_out1
+// Description: A corrected version of the previous adder with carry out.
 
 module add_carry_out1
   #(
@@ -43,15 +69,21 @@ module add_carry_out1
     output logic 	     carry_out
     );
 
-   logic [WIDTH:0] 	     full_sum;   
-   
+   logic [WIDTH:0] 	     full_sum;
+
+   // The corrected version using blocking assignments, causing the accesses
+   // to full_sum to use the newly updated value.
    always_comb begin
-      full_sum = {1'b0,in0} + in1;     
+      full_sum = in0 + in1;     
       sum = full_sum[WIDTH-1:0];
       carry_out = full_sum[WIDTH];      
    end
    
 endmodule // add_carry_out1
+
+
+// Module: add_carry_out2
+// Description: An simplified version of the previous module.
 
 module add_carry_out2
   #(
@@ -63,10 +95,19 @@ module add_carry_out2
     output logic 	     carry_out
     );
 
+   // Instead of declaring a separate wider variable and then slicing into it,
+   // we can instead use the concantenation operator on the LHS of the
+   // assignment. SV chooses the width of an operatoin based on the max of the
+   // operand widths and the result width. Since the result is 1 bit wider,
+   // SV will automatically 0 extend the operands to match.
    assign {carry_out, sum} = in0 + in1;
    
 endmodule
 
+
+// Module: add_carry_inout
+// Description: An extended version of the previous adder that also has a
+//              carry in.
 
 module add_carry_inout
   #(
@@ -79,9 +120,20 @@ module add_carry_inout
     output logic 	     carry_out
     );
 
+   // The carry in is automatically extended to match the result width, so
+   // we only need to add it to the previous version.
    assign {carry_out, sum} = in0 + in1 + carry_in;
    
 endmodule
+
+
+// Module: add_carry_inout_overflow
+// Description: An extension of the previous adder to add a signed overflow.
+//   Note that overflow is avoid if the carry bit is preserved. However, in
+//   contexts where the adder is used with other operations that are WIDTH bits,
+//   the overflow output can be useful to detect signed overflow. It has no
+//   useful meaning for unsigned arithmetic, in which case carry should be
+//   checked.
 
 module add_carry_inout_overflow
   #(
@@ -97,8 +149,11 @@ module add_carry_inout_overflow
    assign {carry_out, sum} = {1'b0, in0} + in1 + carry_in;
 
    // Signed overflow occurs if both inputs have the same sign and the output
-   // has a different sign.
-   assign overflow = (in0[WIDTH-1] == in1[WIDTH-1]) && carry_out != in0[WIDTH-1];
+   // has a different sign. Alternatively, we could xor the two highest carry
+   // bits, but in this case we only have access to the carry out, so we use
+   // this other equation. For a ripple carry adder, xoring the carries could
+   // reduce resources because it is a function with 2 inputs instead of 3.
+   assign overflow = (in0[WIDTH-1] == in1[WIDTH-1]) && (carry_out != in0[WIDTH-1]);
       
 endmodule
 
--- priority_encoder/add_tb.sv
+++ priority_encoder/add_tb.sv
@@ -11,6 +11,8 @@ module add_tb;
    add #(.WIDTH(WIDTH)) UUT (.*);
 
    initial begin
+      $timeformat(-9, 0, " ns");
+      
       for (int i=0; i < NUM_TESTS; i++) begin
 	 in0 = $random;
 	 in1 = $random;
@@ -20,8 +22,7 @@ module add_tb;
       end
 
       $display("Tests completed.");      
-   end
-  
+   end  
 endmodule // add_tb
 
 
@@ -38,18 +39,14 @@ module add_carry_out_tb;
    //add_carry_out1 #(.WIDTH(WIDTH)) UUT (.*);
    add_carry_out2 #(.WIDTH(WIDTH)) UUT (.*);
 
-   logic [WIDTH:0] extended_sum;
-   logic 	   correct_carry_out;
-   
    initial begin
+      logic 	   correct_carry_out;
       $timeformat(-9, 0, " ns");
       
       for (int i=0; i < NUM_TESTS; i++) begin
 	 in0 = $random;
 	 in1 = $random;
-	 extended_sum = {1'b0, in0} + in1;
-	 correct_carry_out = extended_sum[WIDTH];
-	 	 
+	 correct_carry_out = {{1'b0, in0} + in1}[WIDTH];
 	 #10;
 	 if (sum != in0 + in1)
 	   $display("ERROR (time %0t): sum = %d instead of %d.", $realtime, sum, in0+in1);
@@ -75,25 +72,60 @@ module add_carry_inout_tb;
 
    add_carry_inout #(.WIDTH(WIDTH)) UUT (.*);
 
-   logic [WIDTH:0] extended_sum;
-   logic 	   correct_carry_out;
-   
    initial begin
+      logic 	   correct_carry_out;      
       $timeformat(-9, 0, " ns");
       
       for (int i=0; i < NUM_TESTS; i++) begin
 	 in0 = $random;
 	 in1 = $random;
-	 carry_in = $random;
-	 
-	 extended_sum = {1'b0, in0} + in1 + carry_in;
-	 correct_carry_out = extended_sum[WIDTH];	 		 
+	 carry_in = $random;	 	 
+	 correct_carry_out = {{1'b0, in0} + in1 + carry_in}[WIDTH];
 	 #10;
 	 if (sum != in0 + in1 + carry_in)
 	   $display("ERROR (time %0t): sum = %d instead of %d.", $realtime, sum, in0+in1+carry_in);
 
 	 if (carry_out != correct_carry_out)
 	   $display("ERROR (time %0t): carry_out = %d instead of %d.", $realtime, carry_out, correct_carry_out); 
+      end
+
+      $display("Tests completed.");      
+   end
+  
+endmodule // add_carry_inout_tb
+
+
+module add_carry_inout_overflow_tb;
+
+   localparam WIDTH = 16;
+   localparam NUM_TESTS = 1000;
+   
+   logic [WIDTH-1:0] in0, in1;
+   logic [WIDTH-1:0] sum;
+   logic 	     carry_out, carry_in, overflow;
+
+   add_carry_inout_overflow #(.WIDTH(WIDTH)) UUT (.*);
+
+   initial begin
+      logic 	   correct_carry_out;
+      logic 	   correct_overflow;      
+      $timeformat(-9, 0, " ns");
+      
+      for (int i=0; i < NUM_TESTS; i++) begin
+	 in0 = $random;
+	 in1 = $random;
+	 carry_in = $random;	 	 
+	 correct_carry_out = {{1'b0, in0} + in1 + carry_in}[WIDTH];
+	 correct_overflow = (in0[WIDTH-1] == in1[WIDTH-1]) && (correct_carry_out != in0[WIDTH-1]);	 
+	 #10;
+	 if (sum != in0 + in1 + carry_in)
+	   $display("ERROR (time %0t): sum = %d instead of %d.", $realtime, sum, in0+in1+carry_in);
+
+	 if (carry_out != correct_carry_out)
+	   $display("ERROR (time %0t): carry_out = %d instead of %d.", $realtime, carry_out, correct_carry_out);
+
+	 if (overflow != correct_overflow)
+	   $display("ERROR (time %0t): overflow = %d instead of %d.", $realtime, overflow, correct_overflow); 
       end
 
       $display("Tests completed.");      
