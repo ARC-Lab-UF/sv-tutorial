dbbf301fd89813808d116c0effc2ece1917f5cec
Greg Stitt <gstitt@ece.ufl.edu>
2021-12-14 21:50:19 +0000

    Polished mult example

--- priority_encoder/mult.sv
+++ priority_encoder/mult.sv
@@ -1,3 +1,11 @@
+// Greg Stitt
+// University of Florida
+
+// Module: mult
+// Description: Implements a multiplier that prevents overflow by producing
+// a product that is twice the width of the inputs. It uses a paremeter to
+// specify the input width, and whether or not the inputs are signed.
+
 module mult
   #(
     parameter INPUT_WIDTH=16,
@@ -11,7 +19,7 @@ module mult
    always_comb begin
       if (IS_SIGNED) 
 	// SV arithmetic operations are unsigned by default, so we have to
-	// explicitly cast both inputs to sign.
+	// explicitly cast both inputs to signed.
 	// The width of an expression is determined automatically as the 
 	// maximum width of all operands and the result. Since the product is 
 	// twice as wide as the operands, the multiplication will already 
@@ -23,6 +31,10 @@ module mult
    end     
 endmodule // mult
 
+
+// Module: mult2:
+// Description: An alternative implementation that uses a generate statement
+// instead of an always block.
 
 module mult2
   #(
@@ -37,8 +49,8 @@ module mult2
    // In this version of the multiplier, we use generate statements and
    // assign statements to accomplish the same functionality without an
    // always block. The two should synthesize identically since the
-   // if condition in the always block is a constant, which would be optimized
-   // to eliminate the unused path.
+   // if condition is a constant, which should be optimized to eliminate the 
+   // unused path.
    generate
       if (IS_SIGNED) begin
 	 assign product = signed'(in0) * signed'(in1);                
@@ -50,7 +62,13 @@ module mult2
 endmodule
 
 
-module mult3
+// Module: mult_high_low
+// Description: An alternative implementation that replaces the INPUT_WIDTH*2
+// bit product with a high and low output. This generally isn't needed since
+// it is easy to slice into the larger product, but is used to illustrate
+// several new constructs.
+
+module mult_high_low
   #(
     parameter INPUT_WIDTH=16,
     parameter logic IS_SIGNED = 1'b0
@@ -59,9 +77,10 @@ module mult3
     input logic [INPUT_WIDTH-1:0]  in0, in1,
     output logic [INPUT_WIDTH-1:0] high, low
     );
-  
+   
    generate
-      logic [INPUT_WIDTH*2-1:0] product;
+      // Use a tempory variable to store the full product.
+      logic [INPUT_WIDTH*2-1:0]    product;
       if (IS_SIGNED) begin	 
 	 always_comb product = signed'(in0) * signed'(in1);
       end
@@ -69,14 +88,19 @@ module mult3
 	 always_comb product = in0 * in1;
       end
 
+      // Slice into the product to get the high and low outputs.
       assign high = product[INPUT_WIDTH*2-1:INPUT_WIDTH];
       assign low = product[INPUT_WIDTH-1:0];      
-            
+      
    endgenerate  
 endmodule
 
 
-module mult4
+// Module: mult_high_low2
+// Description: An alternative implementation that simplifies the previous
+// version by using concatenation on the outputs.
+
+module mult_high_low2
   #(
     parameter INPUT_WIDTH=16,
     parameter logic IS_SIGNED = 1'b0
@@ -88,6 +112,8 @@ module mult4
 
    generate
       if (IS_SIGNED) begin
+	 // Use concatenation on the outputs to avoid the extra variable. This
+	 // synthesizes to the exact same circuit, but is more concise.
 	 assign {high,low} = signed'(in0) * signed'(in1);              
       end
       else begin
---
+++ priority_encoder/mult_tb.sv
@@ -0,0 +1,81 @@
+// Greg Stitt
+// University of Florida
+
+// Module: mult_tb
+// Description: Testbench for the different mult modules in mult.sv.
+
+module mult_tb;
+
+   localparam INPUT_WIDTH = 16;
+   localparam NUM_TESTS = 1000;
+   
+   logic [INPUT_WIDTH-1:0] in0, in1;
+   logic [INPUT_WIDTH*2-1:0] product_signed, product_unsigned;
+
+   // TODO: Change comments to test different modules.
+   // Apparently SV doesn't allow the .* operator for parameters, only ports.
+   //mult #(.IS_SIGNED(1'b1), .INPUT_WIDTH(INPUT_WIDTH)) UUT_SIGNED (.product(product_signed), .*);
+   //mult #(.IS_SIGNED(1'b0), .INPUT_WIDTH(INPUT_WIDTH)) UUT_UNSIGNED (.product(product_unsigned), .*);
+   mult2 #(.IS_SIGNED(1'b1), .INPUT_WIDTH(INPUT_WIDTH)) UUT_SIGNED (.product(product_signed), .*);
+   mult2 #(.IS_SIGNED(1'b0), .INPUT_WIDTH(INPUT_WIDTH)) UUT_UNSIGNED (.product(product_unsigned), .*);
+
+   initial begin
+
+      logic [INPUT_WIDTH*2-1:0] correct_product_signed, correct_product_unsigned;
+          
+      for (int i=0; i < NUM_TESTS; i++) begin
+	 in0 = $random;
+	 in1 = $random;
+	 correct_product_signed = signed'(in0) * signed'(in1);
+	 correct_product_unsigned = in0 * in1;	 
+	 #10;
+	 if (product_signed != correct_product_signed)
+	    $display("ERROR (time %0t): signed product = %d instead of %d.", $realtime, product_signed, correct_product_signed);
+
+	 if (product_unsigned != correct_product_unsigned)
+	    $display("ERROR (time %0t): unsigned product = %d instead of %d.", $realtime, product_unsigned, correct_product_unsigned);    		 
+      end
+   end      
+endmodule // mult_tb
+
+
+// Module: mult_high_low_tb
+// Description: Testbench for the different mult_high_low modules in mult.sv.
+
+module mult_high_low_tb;
+
+   localparam INPUT_WIDTH = 16;
+   localparam NUM_TESTS = 1000;
+   
+   logic [INPUT_WIDTH-1:0] in0, in1;
+   logic [INPUT_WIDTH-1:0] high_signed, low_signed;
+   logic [INPUT_WIDTH-1:0] high_unsigned, low_unsigned;
+
+   // TODO: Change comments to test different modules.
+   // Apparently SV doesn't allow the .* operator for parameters, only ports.
+   mult_high_low #(.IS_SIGNED(1'b1), .INPUT_WIDTH(INPUT_WIDTH)) UUT_SIGNED (.high(high_signed), .low(low_signed), .*);
+   mult_high_low #(.IS_SIGNED(1'b0), .INPUT_WIDTH(INPUT_WIDTH)) UUT_UNSIGNED (.high(high_unsigned), .low(low_unsigned), .*);
+   //mult_high_low2 #(.IS_SIGNED(1'b1), .INPUT_WIDTH(INPUT_WIDTH)) UUT_SIGNED (.high(high_signed), .low(low_signed), .*);
+   //mult_high_low2 #(.IS_SIGNED(1'b0), .INPUT_WIDTH(INPUT_WIDTH)) UUT_UNSIGNED (.high(high_unsigned), .low(low_unsigned), .*);
+
+   initial begin
+
+      logic [INPUT_WIDTH*2-1:0] product_signed, product_unsigned, correct_product_signed, correct_product_unsigned;
+          
+      for (int i=0; i < NUM_TESTS; i++) begin
+	 in0 = $random;
+	 in1 = $random;
+	 correct_product_signed = signed'(in0) * signed'(in1);
+	 correct_product_unsigned = in0 * in1;	 
+	 #10;
+	 product_signed = {high_signed, low_signed};
+	 product_unsigned = {high_unsigned, low_unsigned};	 
+	 if (product_signed != correct_product_signed)
+	    $display("ERROR (time %0t): signed product = %d instead of %d.", $realtime, product_signed, correct_product_signed);
+
+	 if (product_unsigned != correct_product_unsigned)
+	    $display("ERROR (time %0t): unsigned product = %d instead of %d.", $realtime, product_unsigned, correct_product_unsigned);    		 
+      end
+   end  
+endmodule // mult_high_low_tb
+
