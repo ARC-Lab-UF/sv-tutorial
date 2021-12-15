a92b6bde950d6d4b0ba186cd06e06cef7611913b
Greg Stitt <gstitt@ece.ufl.edu>
2021-12-15 15:09:14 +0000

    Initial register and testbench.

---
+++ sequential/register.sv
@@ -0,0 +1,147 @@
+// Greg Stitt
+// University of Florida
+
+// Module: register_async_rst
+// Description: Implements a register with an active high, asynchronous reset.
+
+module register_async_rst
+  #(
+    parameter WIDTH
+    )
+   (
+    input logic 	     clk,
+    input logic 	     rst,
+    input logic [WIDTH-1:0]  in,
+    output logic [WIDTH-1:0] out
+    );
+
+   // Sequential logic should usually use the always_ff block. For a register
+   // or flip flop with an asynchronous reset, we want to update the output
+   // on any rising clock edge or reset edge.
+   always_ff @(posedge clk or posedge rst) begin
+      
+      // Checking reset first gives it priority over the clock edge, which is
+      // what we want for an asynchronous reset.
+      if (rst)
+	out <= '0;
+      else
+	// This else corresponds to anything on a rising clock edge.
+	// SYNTHESIS RULE: Any non-blocking assignment on a rising clock edge
+	// will synthesize into a register. Blocking assignments can be mixed
+	// with non-blocking assignments, but will cause warnings in always_ff
+	// blocks because they don't always synthesize to a register.
+	out <= in;
+   end
+
+   // NOTE: This could have been implemented as:
+   //    always @(posedge clk or posedge rst) begin
+   // However, always_ff applies additional checks to ensure that everything
+   // assigned in the process is synthesized as a flip flop. When mixing
+   // sequential logic with combinational logic, a normal always block is more
+   // appropriate.
+
+endmodule // register_async_rst
+
+
+// Module: register_sync_rst
+// Description: Implements a register with an active high, synchronous reset.
+
+module register_sync_rst
+  #(
+    parameter WIDTH
+    )
+   (
+    input logic 	     clk,
+    input logic 	     rst,
+    input logic [WIDTH-1:0]  in,
+    output logic [WIDTH-1:0] out
+    );
+
+   // For a synchronous reset, we only want to update outputs a clock edge.
+   always_ff @(posedge clk) begin
+      // The inside of all the always block is identical, but now the reset is
+      // synchronous because this if statement will only ever happen on a clock
+      // edge.
+      if (rst)
+	out <= '0;
+      else
+	out <= in;
+   end
+endmodule // register_sync_rst
+
+
+// Module: register_en_async_rst
+// Description: Implements a register with an active high, asynchronous reset
+// and an enable signal.
+
+module register_en_async_rst
+  #(
+    parameter WIDTH
+    )
+   (
+    input logic 	     clk,
+    input logic 	     rst,
+    input logic 	     en,
+    input logic [WIDTH-1:0]  in,
+    output logic [WIDTH-1:0] out
+    );
+   
+   always_ff @(posedge clk or posedge rst) begin
+      if (rst)
+	out <= '0;
+
+      // Registers typically have an enable or a load signal which must be
+      // asserted for the output to change. We can implement this by simply
+      // checking if the enable is asserted on a rising clock edge.
+      else if (en)
+	out <= in;    	
+   end 
+   
+endmodule // register_en_async_rst
+
+
+// Module: register
+// Description: Implements a parameterized register with a configuration 
+// parameters for width, type of reset, reset activation level, and reset value.
+
+module register
+  #(
+    parameter WIDTH = 16,
+    parameter logic HAS_ASYNC_RESET = 1'b1,
+    parameter logic RESET_ACTIVATION_LEVEL = 1'b1,
+    parameter logic [WIDTH-1:0] RESET_VALUE = '0
+    )
+   (
+    input logic 	     clk,
+    input logic 	     rst,
+    input logic 	     en,
+    input logic [WIDTH-1:0]  in,
+    output logic [WIDTH-1:0] out
+    );
+
+   // Use generate statements to allow for a fully parameterized register
+   generate
+      if (HAS_ASYNC_RESET) begin
+	 // Create an async reset.
+	 always_ff @(posedge clk or posedge rst) begin
+	    // Explicitly check the activation level of the reset to support
+	    // active high or low.
+	    if (rst == RESET_ACTIVATION_LEVEL)
+	      // Use the reset value parameter instead of 0.
+	      out <= RESET_VALUE;
+	    else if (en)
+	      out <= in;    	
+	 end
+      end
+      else begin
+	 // Create an sync reset.
+	 always_ff @(posedge clk) begin
+	    if (rst == RESET_ACTIVATION_LEVEL)
+	      out <= RESET_VALUE;	      
+	    else if (en)
+	      out <= in;
+	 end	
+      end
+   endgenerate   
+endmodule // reg
+
---
+++ sequential/register_tb.sv
@@ -0,0 +1,141 @@
+// Greg Stitt
+// University of Florida
+
+`timescale 1ns / 10 ps
+
+// Module: register_tb
+// Description: Testbench for the register module. This illustrates how to
+// generate a clock signal, how to change inputs every cycle, how to check
+// for correct outputs every cycle, and how to terminate the simulation.
+
+module register_tb;
+
+   localparam NUM_TESTS = 1000;   
+   localparam WIDTH = 8;
+   logic clk = 1'b0, rst, en;   
+   logic [WIDTH-1:0] in, out;
+   
+   register #(.WIDTH(WIDTH)) UUT (.*);
+
+   // Generate a clock with a 10 ns period
+   always begin : generate_clock
+      #5 clk = ~clk;
+   end
+   
+   initial begin : drive_inputs
+      $timeformat(-9, 0, " ns");
+
+      // Reset the register
+      rst = 1'b1;
+      in = '0;      
+      en = 1'b0;
+
+      // Wait 5 cycles
+      for (int i=0; i < 5; i++)
+	@(posedge clk);
+
+      // Clear reset
+      rst = 1'b0;
+
+      // Generate NUM_TESTS random inputs, once per cycle
+      for (int i=0; i < NUM_TESTS; i++) begin	 
+	 in = $random;
+	 en = $random;	 
+	 @(posedge clk);
+      end
+      
+      $display("Tests completed.");
+      // Stops the simulation. $finish can also be used, but will actually
+      // exit the simulator, which isn't appropriate when using a GUI.
+      $stop;             
+   end 
+
+   logic [WIDTH-1:0] prev_out, prev_in, prev_en;
+   
+   always begin : output_check
+      // On each rising edge, save the previous input and output values.
+      @(posedge clk);
+      prev_en = en;
+      prev_in = in;
+      prev_out = out;
+
+      // Give the output time to change
+      @(negedge clk);
+
+      // If enable was asserted, out should be equal to the previous in.
+      if (prev_en && prev_in != out)
+	$display("ERROR (time %0t): out = %d instead of %d.", $realtime, out, prev_in);
+
+      // If enable wasn't asserted, the output shouldn't change.
+      if (!prev_en && out != prev_out)
+	$display("ERROR (time %0t): out = %d instead of %d.", $realtime, out, prev_out);      
+   end   
+endmodule // register_tb
+
+
+// Module: register_tb2
+// Description: A modification of the previous testbench to illustrate how to
+// terminate simulation without use of $stop or $finish.
+
+module register_tb2;
+
+   localparam NUM_TESTS = 1000;   
+   localparam WIDTH = 8;
+   logic clk = 1'b0, rst, en;   
+   logic [WIDTH-1:0] in, out;
+   
+   register #(.WIDTH(WIDTH)) UUT (.*);
+
+   // Here we change the always block to an initial block with an infinite loop.
+   // We do this because we will later use a disable statement, which is similar
+   // to a break statement. The disable will move execution to the end of the
+   // initial block, which breaks the loop.
+   initial begin : generate_clock
+      while(1)
+	#5 clk = !clk;
+   end
+   
+   initial begin : drive_inputs
+      $timeformat(-9, 0, " ns");            
+      rst = 1'b1;
+      in = '0;      
+      en = 1'b0;
+      
+      for (int i=0; i < 5; i++)
+	@(posedge clk);
+
+      rst = 1'b0;
+
+      for (int i=0; i < NUM_TESTS; i++) begin	 
+	 in = $random;
+	 en = $random;
+	 @(posedge clk);
+      end
+
+      // disable the other initial blocks so that the simulation terminates
+      disable generate_clock;
+      disable check_output;            
+      $display("Tests completed.");
+   end 
+
+   logic [WIDTH-1:0] prev_out, prev_in, prev_en;
+
+   // This block was also converted from an always block to an initial with
+   // an infinite loop.
+   initial begin : check_output
+      while (1) begin
+	 @(posedge clk);
+	 prev_en = en;
+	 prev_in = in;
+	 prev_out = out;
+	 @(negedge clk);
+
+	 if (prev_en && prev_in != out)
+	   $display("ERROR (time %0t): out = %d instead of %d.", $realtime, out, prev_in);
+
+	 if (!prev_en && out != prev_out)
+	   $display("ERROR (time %0t): out = %d instead of %d.", $realtime, out, prev_out);
+
+      end
+   end   
+endmodule // register_tb2
