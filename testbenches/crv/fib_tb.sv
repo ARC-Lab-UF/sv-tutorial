6923ed94b1a9fc100e884fd04af9ec5c6908517b
Greg Stitt <gstitt@ece.ufl.edu>
2022-01-05 01:37:31 +0000

    Extended fib testbench

--- testbenches/crv/fib_tb.sv
+++ testbenches/crv/fib_tb.sv
@@ -807,7 +807,7 @@ endclass
 
 module fib_tb5;
    
-   localparam NUM_TESTS = 10000;
+   localparam NUM_TESTS = 1000;
    logic 	     clk;
    env2 #(.num_tests(NUM_TESTS)) _env = new;
    
@@ -835,3 +835,284 @@ module fib_tb5;
    assert property (@(posedge _if.clk) disable iff (_if.rst) _if.go && _if.done |=> !_if.done);
      
 endmodule // fib_tb5
+
+
+// TODO illustate tests with multiple test sizes, repeats, different generation
+// strategies (increment vs random).
+
+class generator3;
+   mailbox driver_mailbox;
+   event   driver_done_event;
+
+   function new(mailbox _driver_mailbox, event _driver_done_event);
+      driver_mailbox = _driver_mailbox;
+      driver_done_event = _driver_done_event;      
+   endfunction // new
+  
+   task run();
+      fib_item3 item = new;
+
+      forever begin	 
+	 if (!item.randomize()) $display("Randomize failed");
+	 //$display("Time %0t [Generator]: Generating fib input %0d, go=%0b.", $time, item.n, item.go);
+	 driver_mailbox.put(item);
+	 @(driver_done_event);
+      end
+   endtask
+endclass // generator3
+
+
+class driver4;   
+   virtual 	     fib_if vif;
+   mailbox 	     driver_mailbox;
+   mailbox 	     scoreboard_n_mailbox;
+   event 	     driver_done_event;
+
+   function new(virtual fib_if _vif, mailbox _driver_mailbox, 
+		mailbox _scoreboard_n_mailbox, event _driver_done_event);
+      vif = _vif;      
+      driver_mailbox = _driver_mailbox;
+      scoreboard_n_mailbox = _scoreboard_n_mailbox;
+      driver_done_event = _driver_done_event;      
+   endfunction // new
+   
+   task run();
+      logic is_first_test = 1'b1;      
+      logic is_active = 1'b0;
+      $display("Time %0t [Driver]: Driver starting.", $time);
+            
+      forever begin
+	 fib_item3 item;
+
+	 //@(posedge vif.clk);
+	 
+	 // If the circuit is reset at any point, reset the driver state.
+	 while (vif.rst) begin
+	    @(posedge vif.clk);	  
+	    is_first_test = 1'b1;
+	    is_active = 1'b0;	    	    
+	 end
+	 
+	 // Wait for the generator to send an input to drive. Unlike before,
+	 // the generator now delivers inputs every cycle, and includes the
+	 // go signal in order to test assertions of go while the DUT is already
+	 // active.
+	 driver_mailbox.get(item);
+	 //$display("Time %0t [Driver]: Driving n=%0d, go=%0b.", $time, item.n, item.go);
+	 
+	 // For this driver, we drive both the n and go inputs directly from the
+	 // generator.
+	 vif.n = item.n;
+	 vif.go = item.go;
+
+	 // Wait until the next clock edge where the inputs will be seen.
+	 // This is needed here because signals haven't changed yet on the
+	 // current clock cycle. So, if done is about to change, we won't see
+	 // it. That would cause the following code to mistake the DUT as
+	 // being active, which could prevent sending the test to the
+	 // scoreboard.
+	 @(posedge vif.clk);
+	 	 
+	 // If done is asserted and go isn't, or if this is the first_test, 
+	 // then the DUT should be inactive and ready for another test.  
+	 if (vif.done || is_first_test)
+	   is_active = 1'b0;
+	 	 
+	 //if (vif.n == 2)
+	 //  $display("Time %0t [Driver]: DEBUG n=%0d, is_active=%0b, go=%0b, done=%0b.", $time, vif.n, is_active, vif.go, vif.done);
+	 	 
+	 // If the DUT isn't already active, and we get a go signal, we are
+	 // starting a test, so inform the scoreboard. The scoreboard will
+	 // then wait to get the result from the monitor. This strategy allows
+	 // the testbench to test assertions of go that don't correspond to
+	 // the start of a test because the DUT is already active. The DUT
+	 // should ignore these assertions.
+	 if (!is_active && vif.go) begin	    
+	    $display("Time %0t [Driver]: Sending start of test for n=%0d.", $time, item.n);
+	    scoreboard_n_mailbox.put(item);
+	    is_active = 1'b1;	    
+	    is_first_test = 1'b0;
+	 end
+
+	 -> driver_done_event;	 
+      end              
+   endtask       
+endclass
+
+
+class monitor3;
+   virtual 	     fib_if vif;
+   mailbox 	     scoreboard_result_mailbox;
+
+   function new(virtual fib_if _vif, mailbox _scoreboard_result_mailbox);
+      vif = _vif;
+      scoreboard_result_mailbox = _scoreboard_result_mailbox;            
+   endfunction // new
+      
+   task run();
+      $display("Time %0t [Monitor]: Monitor starting.", $time);
+      
+      forever begin
+	 fib_item3 item = new;	
+	 @(posedge vif.done);
+	 // In this version, we only care about the result, because the driver
+	 // already informed the scoreboard of the next n value to check.
+	 item.result = vif.result;
+	 $display("Time %0t [Monitor]: Monitor detected result=%0d.", $time, vif.result);
+	 scoreboard_result_mailbox.put(item);
+      end
+   endtask       
+endclass
+
+
+class scoreboard3 #(int num_tests);
+   mailbox scoreboard_result_mailbox;
+   mailbox scoreboard_n_mailbox;
+   int 	   passed, failed, reference;
+
+   function new(mailbox _scoreboard_n_mailbox, mailbox _scoreboard_result_mailbox);
+      scoreboard_n_mailbox = _scoreboard_n_mailbox;
+      scoreboard_result_mailbox = _scoreboard_result_mailbox;
+   endfunction // new
+   
+   function int fib_reference(int n);
+      int 	     x, y, i, temp;
+      x = 0;
+      y = 1;
+      i = 3;
+      if (n < 2)
+	return 0;      
+      
+      while (i <= n) begin
+	 temp = x+y;
+	 x = y;
+	 y = temp;
+	 i ++;	 
+      end
+      return y;
+      
+   endfunction
+
+   task run();
+      passed = 0;
+      failed = 0;
+
+      for (int i=0; i < num_tests; i++) begin
+	 fib_item3 in_item, out_item;
+	 fib_item3 final_item = new;
+
+	 // First wait until the driver informs us of a new test.
+	 scoreboard_n_mailbox.get(in_item);
+	 $display("Time %0t [Scoreboard]: Received start of test for n=%0d.", $time, in_item.n);
+
+	 // Save the n value, otherwise it will be lost.
+	 final_item.n = in_item.n;
+	 	 
+	 // Then, wait until the monitor tells us that test is complete.
+	 scoreboard_result_mailbox.get(out_item);
+	 $display("Time %0t [Scoreboard]: Received result=%0d for n=%0d.", $time, out_item.result, final_item.n);
+ 
+	 final_item.result = out_item.result;
+
+	 // Get the correct result based on the n value at the start of the
+	 // test.
+	 reference = fib_reference(final_item.n);	 
+	 if (final_item.result == reference) begin
+	    $display("Time %0t [Scoreboard] Test passed for n=%0d", $time, final_item.n);
+	    passed ++;
+	 end
+	 else begin
+	    $display("Time %0t [Scoredboard] Test failed: result = %0d instead of %0d for n=%0d.", $time, final_item.result, reference, final_item.n);
+	    failed ++;	    
+	 end
+      end
+   endtask
+
+   function void report_status();     
+      $display("Tests completed: %0d passed, %0d failed", passed, failed);
+   endfunction   
+   
+endclass // scoreboard3
+
+
+class env3 #(int num_tests);
+
+   generator3 gen;
+   driver4 drv;
+   monitor3 monitor;
+   scoreboard3 #(.num_tests(num_tests)) scoreboard;
+   
+   mailbox scoreboard_n_mailbox;
+   mailbox scoreboard_result_mailbox;
+   mailbox driver_mailbox;
+
+   event   driver_done_event;
+      
+   function new(virtual fib_if vif);      
+      scoreboard_n_mailbox = new;
+      scoreboard_result_mailbox = new;
+      driver_mailbox = new;
+
+      // This is a much less error-prone way to create the environment. In the
+      // previous version, we instaniated each of these classes and then
+      // connected their internal signals within the environment. The risk
+      // with that approach is that we don't get any compiler errors. The errors
+      // will only show up during simulation.
+      // With this new approach, we only construct each instance when we have
+      // all the information we need for it. In other words, every object is
+      // fully initialized upon construction. If we leave out required
+      // parameters from the constructor, we will get compiler errors, which is
+      // what we want. Our goal is to always catch as many problems as possible
+      // at compile time.
+      
+      gen = new(driver_mailbox, driver_done_event);
+      drv = new(vif, driver_mailbox, scoreboard_n_mailbox, driver_done_event);
+      monitor = new(vif, scoreboard_result_mailbox);
+      scoreboard = new(scoreboard_n_mailbox, scoreboard_result_mailbox);
+   endfunction // new
+
+   virtual 	     task run();      
+      fork
+	 gen.run();
+	 drv.run();
+	 monitor.run();
+	 scoreboard.run();	 
+      join_any
+
+      scoreboard.report_status();      
+   endtask // run   
+endclass
+
+
+module fib_tb6;
+   
+   localparam NUM_TESTS = 10000;
+   logic 	     clk;
+   
+   fib_if _if (.clk(clk));   
+   fib DUT (.clk(clk), .rst(_if.rst), .go(_if.go), 
+	    .done(_if.done), .n(_if.n), .result(_if.result));
+
+   // Pass the interface to the constructor so that the environment isn't
+   // created in an invalid state that requires further initialization.
+   env3 #(.num_tests(NUM_TESTS)) _env = new(_if);
+   
+   initial begin : generate_clock
+      clk = 1'b0;
+      while(1) #5 clk = ~clk;
+   end
+   
+   initial begin      
+      $timeformat(-9, 0, " ns");
+      _if.rst = 1'b1;
+      _if.go = 1'b0;      
+      for (int i=0; i < 5; i++) @(posedge clk);
+      _if.rst = 1'b0;
+      @(posedge clk);     
+      _env.run();
+      disable generate_clock;      
+   end
+      
+   assert property (@(posedge _if.clk) disable iff (_if.rst) _if.go && _if.done |=> !_if.done);
+     
+endmodule // fib_tb6
