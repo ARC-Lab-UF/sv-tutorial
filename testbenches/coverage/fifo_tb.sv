d53033a265779e5ed2a133f0dcc239d2c2e9a8a0
Greg Stitt <gstitt@ece.ufl.edu>
2022-03-15 23:06:27 +0000

    Polished

--- testbenches/coverage/fifo_tb.sv
+++ testbenches/coverage/fifo_tb.sv
@@ -1,24 +1,22 @@
 // Greg Stitt
 // University of Florida
-// This file contains several testbenches for the fifo module that illustrate
-// different assertion strategies that can simplify a testbench.
 
-// Module: fifo_tb4
-// Description: This testbench extends the testbenches from the assertions
-// examples with cover properties.
+// Module: fifo_tb
+// Description: This testbench extends the FIFO testbench from the assertions
+// example with cover properties.
 
 module fifo_tb;
 
    localparam WIDTH = 8;
    localparam DEPTH = 16;
    
-   logic 	     clk;
-   logic 	     rst;
-   logic 	     full;
-   logic 	     wr_en;
+   logic             clk;
+   logic             rst;
+   logic             full;
+   logic             wr_en;
    logic [WIDTH-1:0] wr_data;
-   logic 	     empty; 
-   logic 	     rd_en; 
+   logic             empty; 
+   logic             rd_en; 
    logic [WIDTH-1:0] rd_data;
 
    fifo #(.WIDTH(WIDTH), .DEPTH(DEPTH)) DUT (.*);
@@ -30,18 +28,19 @@ module fifo_tb;
 
    initial begin
       $timeformat(-9, 0, " ns");
-      rst = 1'b1;
-      rd_en = 1'b0;
-      wr_en = 1'b0;
-      wr_data = '0;      
+      rst <= 1'b1;
+      rd_en <= 1'b0;
+      wr_en <= 1'b0;
+      wr_data <= '0;      
       for (int i=0; i < 5; i++) @(posedge clk);
-      rst = 1'b0;
+      @(negedge clk);
+      rst <= 1'b0;
 
       for (int i=0; i < 10000; i++) begin
-	 wr_data = $random;
-	 wr_en = $random;
-	 rd_en = $random;
-	 @(posedge clk);	 
+         wr_data <= $random;
+         wr_en <= $random;
+         rd_en <= $random;
+         @(posedge clk);         
       end
 
       disable generate_clock;
@@ -63,7 +62,8 @@ module fifo_tb;
    property check_output;
       int wr_tag;
       logic [WIDTH-1:0] data;
-      @(posedge clk) (DUT.valid_wr, wr_tag=tag, inc_tag(), data=wr_data) |-> first_match(##[1:$] (DUT.valid_rd && serving==wr_tag, inc_serving())) ##0 rd_data==data;
+      
+      @(posedge clk) (wr_en && !full, wr_tag=tag, inc_tag(), data=wr_data) |-> first_match(##[1:$] (rd_en && !empty && serving==wr_tag, inc_serving())) ##1 rd_data==data;
    endproperty
             
    ap_check_output : assert property (check_output);
@@ -93,7 +93,9 @@ module fifo_tb;
 
    // To see the coverage results, you often have to enable it in your specific
    // simulator. For Modelsim, you can start it with vsim -coverage. You also
-   // have to right click the files you want to include for coverage and
+   // might have to right click the files you want to include for coverage and
    // select properties. There is then a coverage tab, with different options
    // you can enable.   
+         
 endmodule
+
