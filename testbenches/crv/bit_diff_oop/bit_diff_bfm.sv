4fa3d48e1b5ab998878b0ad28b24d2d6386a34d3
Greg Stitt <gstitt@ece.ufl.edu>
2022-03-17 19:50:25 +0000

    Polished

--- testbenches/crv/bit_diff_oop/bit_diff_bfm.sv
+++ testbenches/crv/bit_diff_oop/bit_diff_bfm.sv
@@ -1,5 +1,5 @@
 interface bit_diff_bfm #(parameter int WIDTH) (input logic clk);
-   logic 	     rst, go, done;
+   logic             rst, go, done;
    logic [WIDTH-1:0] data;
    logic signed [$clog2(2*WIDTH+1)-1:0] result;
 
@@ -26,10 +26,11 @@ interface bit_diff_bfm #(parameter int WIDTH) (input logic clk);
    
    // Reset the design.
    task automatic reset(int cycles);
-      rst = 1'b1;
-      go = 1'b0;      
+      rst <= 1'b1;
+      go <= 1'b0;      
       for (int i=0; i < cycles; i++) @(posedge clk);
-      rst = 1'b0;
+      @(negedge clk);
+      rst <= 1'b0;
       @(posedge clk);      
    endtask
 
@@ -56,21 +57,21 @@ interface bit_diff_bfm #(parameter int WIDTH) (input logic clk);
       is_active = 1'b0;
             
       forever begin
-	 @(posedge clk);
-	 if (rst) is_active = 1'b0;
-	 else begin	    
-	    if (done) is_active = 1'b0;	    
-	    if (!is_active && go) begin 	       
-	       is_active = 1'b1;
-	       // The event is needed because there will be times in the
-	       // simulation where go and done are asserted at the same time.
-	       // If the code simply used @(posedge is_active) to detect the
-	       // start of a test, it would miss these instances because 
-	       // there wouldn't be a rising edge on is_active. It would simply
-	       // remain active between two consecutive tests.
-	       -> active_event;	       
-	    end
-	 end
+         @(posedge clk);
+         if (rst) is_active = 1'b0;
+         else begin         
+            if (done) is_active = 1'b0;     
+            if (!is_active && go) begin                
+               is_active = 1'b1;
+               // The event is needed because there will be times in the
+               // simulation where go and done are asserted at the same time.
+               // If the code simply used @(posedge is_active) to detect the
+               // start of a test, it would miss these instances because 
+               // there wouldn't be a rising edge on is_active. It would simply
+               // remain active between two consecutive tests.
+               -> active_event;        
+            end
+         end
       end      
    endtask // monitor
 endinterface
