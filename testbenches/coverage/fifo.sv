075e9873e7d0ac870f0f2f296cd96e6101cdeee2
Greg Stitt <gstitt@ece.ufl.edu>
2025-01-29 23:58:37 +0000

    Polished

--- testbenches/coverage/fifo.sv
+++ testbenches/coverage/fifo.sv
@@ -1,52 +1,53 @@
 // Greg Stitt
 // University of Florida
-// This file provides a basic FIFO module with a 1-cycle read latency.
+// This file provides a basic FIFO module with a 1-cycle read latency. Note that
+// it includes assertions at the bottom of the module.
 
-module fifo
-  #(
-    parameter WIDTH,
-    parameter DEPTH
-    )
-   (
-    input logic              clk,
-    input logic              rst,
+module fifo #(
+    parameter int WIDTH = 16,
+    parameter int DEPTH = 32
+) (
+    input  logic             clk,
+    input  logic             rst,
     output logic             full,
-    input logic              wr_en,
-    input logic [WIDTH-1:0]  wr_data,
-    output logic             empty, 
-    input logic              rd_en,
-    output logic [WIDTH-1:0] rd_data  
-    );
+    input  logic             wr_en,
+    input  logic [WIDTH-1:0] wr_data,
+    output logic             empty,
+    input  logic             rd_en,
+    output logic [WIDTH-1:0] rd_data
+);
+    logic [WIDTH-1:0] ram[DEPTH];
+    logic valid_wr, valid_rd;
 
-   localparam int READ_LATENCY = 1;
-   
-   logic [WIDTH-1:0]         ram[DEPTH];
-   logic                     valid_wr, valid_rd;
+    localparam int ADDR_WIDTH = $clog2(DEPTH) + 1;
+    logic [ADDR_WIDTH-1:0] wr_addr_r, rd_addr_r;
 
-   localparam int            ADDR_WIDTH = $clog2(DEPTH)+1;
-   logic [ADDR_WIDTH-1:0]   wr_addr_r, rd_addr_r;
+    always_ff @(posedge clk) begin
+        if (valid_wr) ram[wr_addr_r[ADDR_WIDTH-2:0]] <= wr_data;
+        rd_data <= ram[rd_addr_r[ADDR_WIDTH-2:0]];
+    end
 
-   always_ff @(posedge clk) begin
-      if (valid_wr) ram[wr_addr_r[ADDR_WIDTH-2:0]] <= wr_data;
-      rd_data <= ram[rd_addr_r[ADDR_WIDTH-2:0]];      
-   end
-      
-   always_ff @(posedge clk or posedge rst) begin
-      if (rst) begin
-         rd_addr_r <= '0;
-         wr_addr_r <= '0;
-      end
-      else begin         
-         if (valid_wr) wr_addr_r <= wr_addr_r + 1'b1;
-         if (valid_rd) rd_addr_r <= rd_addr_r + 1'b1;
-      end
-   end 
-      
-   assign valid_wr = wr_en && !full;
-   assign valid_rd = rd_en && !empty;
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            rd_addr_r <= '0;
+            wr_addr_r <= '0;
+        end else begin
+            if (valid_wr) wr_addr_r <= wr_addr_r + 1'b1;
+            if (valid_rd) rd_addr_r <= rd_addr_r + 1'b1;
+        end
+    end
 
-   assign full = rd_addr_r[ADDR_WIDTH-2:0] == wr_addr_r[ADDR_WIDTH-2:0] && rd_addr_r[ADDR_WIDTH-1] != wr_addr_r[ADDR_WIDTH-1];
+    assign valid_wr = wr_en && !full;
+    assign valid_rd = rd_en && !empty;
 
-   assign empty = rd_addr_r == wr_addr_r;
-      
+    assign full = rd_addr_r[ADDR_WIDTH-2:0] == wr_addr_r[ADDR_WIDTH-2:0] && rd_addr_r[ADDR_WIDTH-1] != wr_addr_r[ADDR_WIDTH-1];
+    assign empty = rd_addr_r == wr_addr_r;
+
+    // For important properties of a module, it is often a good idea to include
+    // assertions in the synthesizable code. These will be ignored by synthesis
+    // tools, but will still work in simulation. Note that some synthesis tools
+    // might require an attribute or annotation to ignore this.
+    assert property (@(posedge clk) valid_wr |-> !full);
+    assert property (@(posedge clk) valid_rd |-> !empty);
+
 endmodule
--- testbenches/coverage/fifo_tb.sv
+++ testbenches/coverage/fifo_tb.sv
@@ -3,99 +3,106 @@
 
 // Module: fifo_tb
 // Description: This testbench extends the FIFO testbench from the assertions
-// example with cover properties.
+// example with basic cover properties.
 
-module fifo_tb;
+module fifo_tb #(
+    parameter int NUM_TESTS = 10000,
+    parameter int WIDTH = 8,
+    parameter int DEPTH = 16
+);
+    logic             clk;
+    logic             rst;
+    logic             full;
+    logic             wr_en;
+    logic [WIDTH-1:0] wr_data;
+    logic             empty;
+    logic             rd_en;
+    logic [WIDTH-1:0] rd_data;
 
-   localparam WIDTH = 8;
-   localparam DEPTH = 16;
-   
-   logic             clk;
-   logic             rst;
-   logic             full;
-   logic             wr_en;
-   logic [WIDTH-1:0] wr_data;
-   logic             empty; 
-   logic             rd_en; 
-   logic [WIDTH-1:0] rd_data;
+    fifo #(
+        .WIDTH(WIDTH),
+        .DEPTH(DEPTH)
+    ) DUT (
+        .*
+    );
 
-   fifo #(.WIDTH(WIDTH), .DEPTH(DEPTH)) DUT (.*);
-   
-   initial begin : generate_clock
-      clk = 1'b0;
-      while(1) #5 clk = ~clk;
-   end
+    initial begin : generate_clock
+        clk <= 1'b0;
+        forever #5 clk <= ~clk;
+    end
 
-   initial begin
-      $timeformat(-9, 0, " ns");
-      rst <= 1'b1;
-      rd_en <= 1'b0;
-      wr_en <= 1'b0;
-      wr_data <= '0;      
-      for (int i=0; i < 5; i++) @(posedge clk);
-      @(negedge clk);
-      rst <= 1'b0;
+    initial begin
+        rst     <= 1'b1;
+        rd_en   <= 1'b0;
+        wr_en   <= 1'b0;
+        wr_data <= '0;
+        repeat (5) @(posedge clk);
+        @(negedge clk);
+        rst <= 1'b0;
 
-      for (int i=0; i < 10000; i++) begin
-         wr_data <= $random;
-         wr_en <= $random;
-         rd_en <= $random;
-         @(posedge clk);         
-      end
+        for (int i = 0; i < NUM_TESTS; i++) begin
+            wr_data <= $urandom;
+            wr_en   <= $urandom;
+            rd_en   <= $urandom;
+            @(posedge clk);
+        end
 
-      disable generate_clock;
-      $display("Tests Completed.");
-   end
-      
-   assert property (@(posedge clk) DUT.valid_wr |-> !full);
-   assert property (@(posedge clk) DUT.valid_rd |-> !empty);
+        disable generate_clock;
+        $display("Tests Completed.");
+    end
 
-   int tag=0, serving=0;   
-   function void inc_tag();
-      tag = tag + 1'b1;
-   endfunction
-   
-   function void inc_serving();
-      serving = serving + 1'b1; 
-   endfunction
-   
-   property check_output;
-      int wr_tag;
-      logic [WIDTH-1:0] data;
-      
-      @(posedge clk) (wr_en && !full, wr_tag=tag, inc_tag(), data=wr_data) |-> first_match(##[1:$] (rd_en && !empty && serving==wr_tag, inc_serving())) ##1 rd_data==data;
-   endproperty
-            
-   ap_check_output : assert property (check_output);
+    assert property (@(posedge clk) DUT.valid_wr |-> !full);
+    assert property (@(posedge clk) DUT.valid_rd |-> !empty);
 
-   // At a bare minimum, we want to make sure that the testbench actually tests
-   // a write and a read. This is virtually guaranteed based on the number of
-   // tests performed above, but this is how we actually verify it.
-   //
-   // To ensure a specific test happens (i.e., is "covered"), we can use cover
-   // properties. Cover properties work similarly to assertion
-   // properties, except the simulator only tracks the number of times that
-   // the property passes. Basically, simulators ignore when assertions pass
-   // and when covers fail.
-   //
-   // So, in these cases, the simulator would tell us the number of writes and
-   // reads that were performed. If that number is > 0, we have at least covered
-   // these basic tests.
-   cp_write : cover property (@(posedge clk) wr_en);
-   cp_read : cover property (@(posedge clk) rd_en);
+    logic [WIDTH-1:0] correct_rd_data;
+    logic [WIDTH-1:0] model_queue[$];
 
-   // We should also check to make sure we test the FIFO for writes when the
-   // FIFO is full, and reads when the FIFO is empty. Although either of these
-   // would usually be an error by the user of the FIFO, we want to make sure
-   // that incorrect usage does not corrupt the internal state of the FIFO.
-   cp_write_while_full : cover property (@(posedge clk) wr_en && full);
-   cp_read_while_empty : cover property (@(posedge clk) rd_en && empty);
+    // Model the functionality of the FIFO with a queue.
+    always_ff @(posedge clk or posedge rst)
+        if (rst) model_queue = {};
+        else begin
+            automatic int size = model_queue.size();
+            if (rd_en && size > 0) correct_rd_data <= model_queue.pop_front();
+            if (wr_en && size < DEPTH) model_queue.push_back(wr_data);
+        end
 
-   // To see the coverage results, you often have to enable it in your specific
-   // simulator. For Modelsim, you can start it with vsim -coverage. You also
-   // might have to right click the files you want to include for coverage and
-   // select properties. There is then a coverage tab, with different options
-   // you can enable.   
-         
+    assert property (@(posedge clk) rd_en && model_queue.size() > 0 |=> rd_data == correct_rd_data);
+
+    // At a bare minimum, we want to make sure that the testbench actually tests
+    // a write and a read. This is virtually guaranteed based on the number of
+    // tests performed above, but this is how we actually verify it.
+    //
+    // To ensure a specific test happens (i.e., is "covered"), we can use cover
+    // properties. Cover properties work similarly to assertion
+    // properties, except the simulator only tracks the number of times that
+    // the property passes. Basically, simulators ignore when assertions pass
+    // and when covers fail.
+    //
+    // So, in these cases, the simulator would tell us the number of writes and
+    // reads that were performed. If that number is > 0, we have at least covered
+    // these basic tests.
+
+    // NOTE: My formatter is putting the labels on different lines. I don't 
+    // recommend that.
+    cp_write :
+    cover property (@(posedge clk) wr_en);
+    cp_read :
+    cover property (@(posedge clk) rd_en);
+
+    // We should also check to make sure we test the FIFO for writes when the
+    // FIFO is full, and reads when the FIFO is empty. Although either of these
+    // would usually be an error by the user of the FIFO, we want to make sure
+    // that incorrect usage does not corrupt the internal state of the FIFO.
+    cp_write_while_full :
+    cover property (@(posedge clk) wr_en && full);
+    cp_read_while_empty :
+    cover property (@(posedge clk) rd_en && empty);
+
+    // To see the coverage results, you often have to enable it in your specific
+    // simulator. For Questa, you can start it with vsim -coverage. You also
+    // might have to right click the files you want to include for coverage and
+    // select properties. There is then a coverage tab, with different options
+    // you can enable.   
+
 endmodule
 
