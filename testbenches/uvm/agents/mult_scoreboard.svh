f467f6a459fcd9da9977d6e0b094febe7fa147c8
Greg Stitt <gstitt@ece.ufl.edu>
2025-02-09 21:27:55 +0000

    Polished

--- testbenches/uvm/agents/mult_scoreboard.svh
+++ testbenches/uvm/agents/mult_scoreboard.svh
@@ -1,10 +1,8 @@
 // Greg Stitt
 // University of Florida
 
-// The scoreboard waits to receive an input from the start monitor and the 
-// actual output from the done monitor. It then uses the input to compute the
-// expected output. Finally, it compares the expected output with the actual
-// output, and reports errors if there are any differences.
+// This scoreboard uses analysis exports and FIFOs to receive data from the
+// different agent monitors.
 
 `ifndef _MULT_SCOREBOARD_SVH_
 `define _MULT_SCOREBOARD_SVH_
@@ -14,13 +12,22 @@ import uvm_pkg::*;
 
 import mult_tb_pkg::*;
 
+// The scoreborad is not parameterized. It only needs a single input width, so
+// it is easier to just get that from mult_tb_pkg. It could easily parameterized
+// with the environment providing the parameter, but there is no real advantage
+// to doing so.
 class mult_scoreboard extends uvm_scoreboard;
     `uvm_component_utils(mult_scoreboard)
 
+    // Analysis exports (sinks of analysis ports)
     uvm_analysis_export #(logic [mult_tb_pkg::INPUT_WIDTH-1:0]) in0_ae;
     uvm_analysis_export #(logic [mult_tb_pkg::INPUT_WIDTH-1:0]) in1_ae;
     uvm_analysis_export #(logic [2*mult_tb_pkg::INPUT_WIDTH-1:0]) out_ae;
 
+    // Analysis FIFOs, used to simply synchronization between analysis
+    // ports and exports. This basically enables the scoreboard to read
+    // from a FIFO, with the FIFO automatically storing monitor data
+    // arriving via the analysis ports.
     uvm_tlm_analysis_fifo #(logic [mult_tb_pkg::INPUT_WIDTH-1:0]) in0_fifo;
     uvm_tlm_analysis_fifo #(logic [mult_tb_pkg::INPUT_WIDTH-1:0]) in1_fifo;
     uvm_tlm_analysis_fifo #(logic [2*mult_tb_pkg::INPUT_WIDTH-1:0]) out_fifo;
@@ -33,22 +40,27 @@ class mult_scoreboard extends uvm_scoreboard;
         passed = 0;
         failed = 0;
 
+        // Get signed status to calculate the expected output.
         if (!uvm_config_db#(bit)::get(this, "", "is_signed", is_signed)) `uvm_fatal("NO_IS_SIGNED", "is_signed not specified.");
     endfunction
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
+        // Create the analysis exports.
         in0_ae = new("in0_ae", this);
         in1_ae = new("in1_ae", this);
         out_ae = new("out_ae", this);
 
+        // Create the analysis FIFOs.
         in0_fifo = new("in0_fifo", this);
         in1_fifo = new("in1_fifo", this);
         out_fifo = new("out_fifo", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
+        // Connect the FIFOs to the analysis exports. This basically enables
+        // us to simply read from the FIFO to get data send from analsis ports.
         in0_ae.connect(in0_fifo.analysis_export);
         in1_ae.connect(in1_fifo.analysis_export);
         out_ae.connect(out_fifo.analysis_export);
@@ -59,6 +71,8 @@ class mult_scoreboard extends uvm_scoreboard;
         logic [2*INPUT_WIDTH-1:0] actual, expected;
 
         forever begin
+            // Read from the analysis FIFOs to get the two inputs and the
+            // actual output.
             in0_fifo.get(in0);
             in1_fifo.get(in1);
             out_fifo.get(actual);
@@ -67,6 +81,7 @@ class mult_scoreboard extends uvm_scoreboard;
             if (is_signed) expected = signed'(in0) * signed'(in1);
             else expected = in0 * in1;
 
+            // Check for errors.
             if (actual == expected) begin
                 `uvm_info("SCOREBOARD", $sformatf("Test passed for in0=%0d, in1=%0d.", in0, in1), UVM_LOW)
                 passed++;
