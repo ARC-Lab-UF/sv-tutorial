0667b4fb246cd2fa217da1795daa5f3d93c62f01
Greg Stitt <gstitt@ece.ufl.edu>
2025-02-19 22:32:24 +0000

    Polishing

--- testbenches/uvm/accum/accum_sequence.svh
+++ testbenches/uvm/accum/accum_sequence.svh
@@ -15,12 +15,17 @@ import accum_tb_pkg::*;
 virtual class accum_base_sequence extends uvm_sequence #(axi4_stream_seq_item #(accum_tb_pkg::INPUT_WIDTH));
     `uvm_object_utils(accum_base_sequence)
 
-    int num_tests;
+    int num_packets;
     bit is_packet_level;
+
+    int min_packet_size;
+    int max_packet_size;
 
     function new(string name = "accum_sequence");
         super.new(name);
-        if (!uvm_config_db#(int)::get(this, "", "num_tests", num_tests)) `uvm_fatal("NO_NUM_TESTS", "num_tests not specified.");
+        if (!uvm_config_db#(int)::get(this, "", "num_packets", num_packets)) `uvm_fatal("NO_NUM_PACKETS", "num_packets not specified.");
+        if (!uvm_config_db#(int)::get(this, "", "min_packet_size", min_packet_size)) `uvm_fatal("NO_MIN_PACKET_SIZE", "min_packet_size not specified.");
+        if (!uvm_config_db#(int)::get(this, "", "max_packet_size", max_packet_size)) `uvm_fatal("NO_MAX_PACKET_SIZE", "max_packet_size not specified.");
     endfunction
 endclass
 
@@ -34,28 +39,35 @@ class accum_beat_sequence extends accum_base_sequence;
     endfunction
 
     virtual task body();
-        for (int i = 0; i < num_tests; i++) begin
-            req = axi4_stream_seq_item#(accum_tb_pkg::INPUT_WIDTH)::type_id::create($sformatf("req%0d", i));
-            wait_for_grant();
+        int packet_size;
+        int count = 0;
 
-            void'(req.randomize() with {
-                tdata.size() == 1;
-                tdata[0] dist {
-                    '0                                       :/ 2,
-                    '1                                       :/ 2,
-                    [0 : 2 ** accum_tb_pkg::INPUT_WIDTH - 2] :/ 96
-                };
-                tlast dist {
-                    1 :/ 70,
-                    0 :/ 30
-                };
-                tstrb[0] == '1;
-                tkeep[0] == '1;
-                is_packet_level == 1'b0;
-            });
+        for (int i = 0; i < num_packets; i++) begin
 
-            send_request(req);
-            wait_for_item_done();
+            packet_size = $urandom_range(min_packet_size, max_packet_size);
+
+            for (int j = 0; j < packet_size; j++) begin
+
+                req = axi4_stream_seq_item#(accum_tb_pkg::INPUT_WIDTH)::type_id::create($sformatf("req%0d", count++));
+                wait_for_grant();
+
+                assert(req.randomize() with {
+                    tdata.size() == 1;
+                    tdata[0] dist {
+                        '0                                       :/ 2,
+                        '1                                       :/ 2,
+                        [0 : 2 ** accum_tb_pkg::INPUT_WIDTH - 2] :/ 96
+                    };
+                    (j == packet_size-1) -> tlast == 1'b1;
+                    (j < packet_size-1) -> tlast == 1'b0;              
+                    tstrb[0] == '1;
+                    tkeep[0] == '1;
+                    is_packet_level == 1'b0;
+                }) else $fatal(1, "Randomization failed.");
+
+                send_request(req);
+                wait_for_item_done();
+            end
         end
     endtask
 endclass
@@ -70,14 +82,14 @@ class accum_packet_sequence extends accum_base_sequence;
     endfunction
 
     virtual task body();
-        for (int i = 0; i < num_tests; i++) begin
+        for (int i = 0; i < num_packets; i++) begin
             req = axi4_stream_seq_item#(accum_tb_pkg::INPUT_WIDTH)::type_id::create($sformatf("req%0d", i));
             wait_for_grant();
 
             req.is_packet_level = 1'b1;
 
-            assert(req.randomize() with {
-                tdata.size() inside {[1:10]};
+            assert (req.randomize() with {
+                tdata.size() inside {[min_packet_size : max_packet_size]};
                 foreach (tdata[i]) {
                     tdata[i] dist {
                         '0                                       :/ 2,
@@ -85,13 +97,14 @@ class accum_packet_sequence extends accum_base_sequence;
                         [0 : 2 ** accum_tb_pkg::INPUT_WIDTH - 2] :/ 96
                     };
                     tstrb[i] == '1;
-                    tkeep[i] == '1;                    
+                    tkeep[i] == '1;
                 }
                 is_packet_level == 1'b1;
-            }) else $fatal(1, "Randomization failed.");
+            })
+            else $fatal(1, "Randomization failed.");
 
             // tlast is undefined for a packet-level transaction. The driver
-            // will set tlast automatically.
+            // will set tlast automatically when reaching the final beat.
             req.tlast = 1'bX;
 
             send_request(req);
--- testbenches/uvm/accum/accum_tb.sv
+++ testbenches/uvm/accum/accum_tb.sv
@@ -13,7 +13,7 @@ import accum_tb_pkg::*;
 `timescale 1 ns / 100 ps
 
 module accum_tb #(
-    parameter int NUM_TESTS   = 10000,
+    parameter int NUM_PACKETS   = 10000,
 
     // Enable/disable random toggling of the downstream ready signal.
     parameter bit TOGGLE_READY = 1'b1,
@@ -21,7 +21,11 @@ module accum_tb #(
     // This gives us the capability to change the amount of cycles between
     // inputs in the AXI drivers. This is very useful for exposing bugs.
     parameter int MIN_DRIVER_DELAY = 1,
-    parameter int MAX_DRIVER_DELAY = 5
+    parameter int MAX_DRIVER_DELAY = 5,
+
+    // Put constraints on the randomized packet sizes.
+    parameter int MIN_PACKET_SIZE = 1,
+    parameter int MAX_PACKET_SIZE = 20
 );
     bit clk = 1'b0;
     bit rst;
@@ -48,7 +52,8 @@ module accum_tb #(
         .in_tlast  (in_intf.tlast),
         .out_tvalid(out_intf.tvalid),
         .out_tready(out_intf.tready),
-        .out_tdata (out_intf.tdata)
+        .out_tdata (out_intf.tdata),
+        .out_tlast (out_intf.tlast)
     );
 
     // Optionally toggle the ready signal randomly.
@@ -79,11 +84,15 @@ module accum_tb #(
         uvm_config_db#(virtual axi4_stream_if #(accum_tb_pkg::OUTPUT_WIDTH))::set(uvm_root::get(), "*", "out_vif", out_intf);
 
         // Store the number of tests.
-        uvm_config_db#(int)::set(uvm_root::get(), "*", "num_tests", NUM_TESTS);
+        uvm_config_db#(int)::set(uvm_root::get(), "*", "num_packets", NUM_PACKETS);
 
         // Store configuration info for the drivers.
         uvm_config_db#(int)::set(uvm_root::get(), "*", "min_driver_delay", MIN_DRIVER_DELAY);
         uvm_config_db#(int)::set(uvm_root::get(), "*", "max_driver_delay", MAX_DRIVER_DELAY);
+
+        // Store the packet range.
+        uvm_config_db#(int)::set(uvm_root::get(), "*", "min_packet_size", MIN_PACKET_SIZE);
+        uvm_config_db#(int)::set(uvm_root::get(), "*", "max_packet_size", MAX_PACKET_SIZE);
     end
 
     initial begin
