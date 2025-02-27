51ee8978b68ee0b18771f2791942e9f086444109
Greg Stitt <gstitt@ece.ufl.edu>
2025-02-27 11:46:00 +0000

    Polished

--- testbenches/basic/register_tb.sv
+++ testbenches/basic/register_tb.sv
@@ -14,28 +14,32 @@
 
 `timescale 1ns / 100 ps
 
-// Module: register_tb1
-// Description: An overly complex testbench for the register module. This is only
-// demonstrated for explaining common strategies. I do not recommend the
-// strategy presented in this module.
+
+// Module: register_tb2
+// Description: A simple, but non-ideal testbench for the register module. 
+// This is only demonstrated for explaining common non-ideal strategies. I don't 
+// recommend the strategy presented in this module.
 
 module register_tb1;
 
     localparam int NUM_TESTS = 10000;
     localparam int WIDTH = 8;
+
+    // DUT I/O
     logic clk = 1'b0, rst, en;
     logic [WIDTH-1:0] in, out;
-    logic [WIDTH-1:0] prev_in, prev_out, prev_en;
+
+    // Used to help with verification.
+    logic [WIDTH-1:0] prev_out;
 
     register #(.WIDTH(WIDTH)) DUT (.*);
 
     // Generate a clock with a 10 ns period
-    always begin : generate_clock
-        #5 clk <= ~clk;
+    initial begin : generate_clock
+        forever #5 clk <= ~clk;
     end
 
-    // A process for driving the inputs.
-    initial begin : drive_inputs
+    initial begin
         $timeformat(-9, 0, " ns");
 
         // Reset the register. Following the advice from the race-condition 
@@ -63,103 +67,154 @@ module register_tb1;
             in <= $urandom;
             en <= $urandom;
             @(posedge clk);
+
+            // We need to save to previous output to verify that the output
+            // doesn't change when enable isn't asserted.
+            prev_out = out;
+
+            // We need to wait for some amount of time to allow for the 
+            // register's output to change. This has an unattractive side effect
+            // of shifting all the subsequent inputs by this amount of time.
+            // While it still works, I highly recommend against this strategy.
+            // I always try to make all my testbenches do everything on rising
+            // clock edges. While that isn't necessary, it is an excellent
+            // exercise to understand low-level simulation details.
+            #1;
+
+            // Verify the outputs.
+            if (en && in != out) $error("out = %d instead of %d.", out, in);
+            if (!en && out != prev_out) $error("out changed when en wasn't asserted.");
         end
 
         $display("Tests completed.");
-        $stop;
+        disable generate_clock;
     end
 
-    always begin : check_output
-        // Wait for a rising edge to check the output.
-        @(posedge clk);
-
-        // Uncomment to see the value of all signals on each clock edge.
-        //
-        // The inputs are all the previous values due to the writing process
-        // using non-blocking assignments. Use of blocking assignments in the
-        // writing process would cause a race condition that could result in
-        // non-deterministic behavior.
-        //
-        // The output has not yet changed at this point because the register
-        // uses a non-blocking assignment and this $display reads the value in
-        // the same time step. However, even if the circuit wasn't a register and
-        // used a blocking assignment, we would again have a race condition
-        // because it isn't known whether or not the simulator updates the value
-        // of out before it is read here.
-        //
-        // In either case, we have to wait before getting the new value of out.
-        //$display("LOG (time %0t): en = %0b, in = %0d, out = %0d", $time, en, in, out);  
-
-        // Save the previous output value so we can test if en = 0 works.
-        prev_en  = en;
-        prev_in  = in;
-        prev_out = out;
-
-        // Give the output time to change. Any amount of time will work as long 
-        // as it is less than 1 clock cycle.
-        //
-        // IMPORTANT: if you find yourself waiting for a small amount of time
-        // for an output to change (outside of combinational logic), there is 
-        // usually a better way of writing the testbench. Also, ideally youre
-        // testbench shouldn't care if the output is registered or combinational,
-        // so we'll see those better ways in later examples.
-        //
-        //#1;
-        //#0;
-        @(negedge clk);
-
-        // If enable was asserted, out should be equal to the previous in.
-        if (prev_en && prev_in != out) $error("[%0t] out = %d instead of %d.", $time, out, prev_in);
-
-        // If enable wasn't asserted, the output shouldn't change.
-        if (!prev_en && out != prev_out) $error("[%0t] out = %d instead of %d.", $time, out, prev_out);
-    end
 endmodule  // register_tb1
 
 
 // Module: register_tb2
-// Description: A simpler alternative to the previous testbench that also
-// shows how to terminate a simulation without use of $stop or $finish. 
-//
-// This is still not a "good" testbench for a register. We'll see far simpler
-// methods in the later examples.
+// Description: This testbench separates some of the testing responsibilities
+// across to blocks to eliminate the input offsets experienced by the previous
+// testbench. This is still an overly complex testbench for the register module, 
+// which is only demonstrated for explaining commonly attempted strategies. I do
+// not recommend the strategy presented in this module.
 
 module register_tb2;
 
-    localparam NUM_TESTS = 10000;
-    localparam WIDTH = 8;
-    logic clk, rst, en;
+    localparam int NUM_TESTS = 10000;
+    localparam int WIDTH = 8;
+    logic clk = 1'b0, rst, en;
     logic [WIDTH-1:0] in, out;
     logic [WIDTH-1:0] prev_in, prev_out, prev_en;
 
     register #(.WIDTH(WIDTH)) DUT (.*);
 
-    // Here we change the always block to an initial block with an infinite loop.
-    // We do this because we will later use a disable statement, which is similar
-    // to a break statement. The disable will move execution to the end of the
-    // initial block, which breaks the loop.
     initial begin : generate_clock
-        clk = 1'b0;
-        forever #5 clk = !clk;
+        forever #5 clk <= ~clk;
+    end
+
+    // A process for driving the inputs.
+    initial begin : drive_inputs
+        $timeformat(-9, 0, " ns");
+
+        rst <= 1'b1;
+        in  <= '0;
+        en  <= 1'b0;
+        repeat (5) @(posedge clk);
+        @(negedge clk);
+        rst <= 1'b0;
+        @(posedge clk);
+
+        for (int i = 0; i < NUM_TESTS; i++) begin
+            in <= $urandom;
+            en <= $urandom;
+            @(posedge clk);
+        end
+
+        $display("Tests completed.");
+        disable generate_clock;
+    end
+
+    initial begin : check_output
+        forever begin
+            // Wait for a rising edge to check the output.
+            @(posedge clk);
+
+            // Uncomment to see the value of all signals on each clock edge.
+            //
+            // The inputs are all the previous values due to the writing process
+            // using non-blocking assignments. Use of blocking assignments in the
+            // writing process would cause a race condition that could result in
+            // non-deterministic behavior.
+            //
+            // The output has not yet changed at this point because the register
+            // uses a non-blocking assignment and this $display reads the value in
+            // the same time step. However, even if the circuit wasn't a register and
+            // used a blocking assignment, we would again have a race condition
+            // because it isn't known whether or not the simulator updates the value
+            // of out before it is read here.
+            //
+            //$display("LOG (time %0t): en = %0b, in = %0d, out = %0d", $time, en, in, out);  
+
+            // Save the previous input and output values so we can test en == 0.
+            prev_en  = en;
+            prev_in  = in;
+            prev_out = out;
+
+            // Give the output time to change. Any amount of time will work as long 
+            // as it is less than 1 clock cycle.
+            //
+            // IMPORTANT: if you find yourself waiting for a small amount of time
+            // for an output to change (outside of combinational logic), there is 
+            // usually a better way of writing the testbench. Also, ideally youre
+            // testbench shouldn't care if the output is registered or combinational,
+            // so we'll see those better ways in later examples.
+            //
+            //#1;
+            //#0;
+            @(negedge clk);
+
+            // If enable was asserted, out should be equal to the previous in.
+            if (prev_en && prev_in != out) $error("out = %d instead of %d.", out, prev_in);
+
+            // If enable wasn't asserted, the output shouldn't change.
+            if (!prev_en && out != prev_out) $error("out changed when en wasn't asserted.");
+        end
+    end
+endmodule  // register_tb2
+
+
+// Module: register_tb3
+// Description: A simpler alternative to the previous testbench.
+// This is still not a good testbench for a register. We'll see far simpler
+// methods in the later examples.
+
+module register_tb3;
+
+    localparam NUM_TESTS = 10000;
+    localparam WIDTH = 8;
+    logic clk = 1'b0, rst, en;
+    logic [WIDTH-1:0] in, out;
+    logic [WIDTH-1:0] prev_in, prev_out, prev_en;
+
+    register #(.WIDTH(WIDTH)) DUT (.*);
+
+    initial begin : generate_clock
+        forever #5 clk <= !clk;
     end
 
     initial begin : drive_inputs
         $timeformat(-9, 0, " ns");
 
-        // Reset the register
         rst <= 1'b1;
         in  <= '0;
         en  <= 1'b0;
-
-        // Wait 5 cycles
         repeat (5) @(posedge clk);
-
-        // Clear reset
         @(negedge clk);
         rst <= 1'b0;
         @(posedge clk);
 
-        // Generate NUM_TESTS random inputs, once per cycle
         for (int i = 0; i < NUM_TESTS; i++) begin
             in <= $urandom;
             en <= $urandom;
@@ -167,7 +222,7 @@ module register_tb2;
             // Instead of having one process write some signals, and other process
             // write other signals, we can just have one process write and one
             // process read, and make sure to write using non-blocking assignments.
-            // Note that the prev_ version here intentaionally are getting the
+            // Note that the prev_ versions here are intentionally getting the
             // non-updated values by using non-blocking assignments earlier.
             // It is safe to mix blocking and non-blocking assignments in 
             // testbenches, but any blocking assignments must not be to signals
@@ -178,11 +233,8 @@ module register_tb2;
             @(posedge clk);
         end
 
-        // Disable the other initial blocks so that the simulation terminates.
-        disable generate_clock;
-        disable check_output;
-
         $display("Tests completed.");
+        disable generate_clock;
     end
 
     // Since we are tracking the previous values, we can just directly compare
@@ -195,13 +247,15 @@ module register_tb2;
     // Again, if you ever find yourself having to check a value shortly after a 
     // clock edge to get the right value (or to avoid a race condition), there is
     // almost certainly an easier way to do the testbench.
-    always begin : check_output
-        @(posedge clk);
+    initial begin : check_output
+        forever begin
+            @(posedge clk);
 
-        // If enable was asserted, out should be equal to the previous in.
-        if (prev_en && prev_in != out) $error("[%0t] out = %d instead of %d.", $time, out, prev_in);
+            // If enable was asserted, out should be equal to the previous in.
+            if (prev_en && prev_in != out) $error("out = %d instead of %d.", out, prev_in);
 
-        // If enable wasn't asserted, the output shouldn't change.
-        if (!prev_en && out != prev_out) $error("[%0t] out = %d instead of %d.", $time, out, prev_out);
+            // If enable wasn't asserted, the output shouldn't change.
+            if (!prev_en && out != prev_out) $error("out changed when en wasn't asserted.");
+        end
     end
-endmodule  // register_tb2
+endmodule  // register_tb3
